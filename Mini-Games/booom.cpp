97b9f4ed2a15eaa8b74d0566412e5c9700d20929
EnderBill <48132629+EnderBill@users.noreply.github.com>
2022-05-04 10:30:14 +0800

    5.4.10.29
    Booom遊戲完善

--- Mini-Games/booom.cpp
+++ Mini-Games/booom.cpp
@@ -3,12 +3,25 @@
 
 #include <QString>
 #include <QTime>
+#include <QMessageBox>
 
 int Booom::BoomSize=0;
 int Booom::BoomMax=0;
 int Booom::BoomMin=0;
 int Booom::BoomSec=0;
 int Booom::BoomEnd=0;
+
+QString Booom::play="";
+
+int Booom::playerType_1=0;
+int Booom::playerType_2=0;
+int Booom::playerType_3=0;
+int Booom::playerType_4=0;
+
+QString Booom::playName_1="";
+QString Booom::playName_2="";
+QString Booom::playName_3="";
+QString Booom::playName_4="";
 
 Booom::Booom(QWidget *parent) :
     QWidget(parent),
@@ -24,10 +37,13 @@ Booom::~Booom()
 }
 
 void Booom::setBoomKeySize(){
-    ui->BoomKeySize->setText(
+    ui->BoomKeySize->
+            setText(
                 QString::number(BoomMin)+
                 tr("~")+
-                QString::number(BoomMax));
+                QString::number(BoomMax)
+                );
+
     int Value=100-((BoomMax-BoomMin)*100+0.1)/BoomSize;
     ui->BoomBar->setValue(Value);
 }
@@ -35,28 +51,84 @@ void Booom::setBoomKeySize(){
 void Booom::BoomReset(){
     ui->BooomPage->setCurrentIndex(0);
     ui->BoomKeySize->setFocusPolicy(Qt::NoFocus);
+    ui->playerName_1->setFocusPolicy(Qt::NoFocus);
+    ui->playerName_2->setFocusPolicy(Qt::NoFocus);
+    ui->playerName_3->setFocusPolicy(Qt::NoFocus);
+    ui->playerName_4->setFocusPolicy(Qt::NoFocus);
     ui->BoomKey->setText("");
     ui->BoomSizeKey->setText("");
     ui->BoomSecKey->setText("");
+
+    ui->radioButton_3->setChecked(1);
+    ui->radioButton_7->setChecked(1);
+    ui->radioButton_11->setChecked(1);
+    ui->radioButton_15->setChecked(1);
+
+    nameSet_1(2);
+    nameSet_2(2);
+    nameSet_3(2);
+    nameSet_4(2);
+
     BoomMin=0;
     setBoomKeySize();
+    ui->BoomSizeKey->setFocus();
 }
 
 void Booom::Boooom(){
+    QString out=play;
+    out+=" failed to defuse the bomb";
+    QMessageBox:: critical ( this , tr ( "Boooom" ), out);
     BoomReset();
 }
 
-void Booom::KeyIn(int NewKey)
-{
-    if(BoomMin<NewKey&&NewKey<BoomMax){
+void Booom::WIN(){
+    QString out="The bomb was dismantled successfully, all players win";
+    QMessageBox:: about ( this , tr ( "Boooom" ), out);
+    BoomReset();
+}
 
-       if(BoomEnd==NewKey)Boooom();
-       else {
-           if(NewKey>BoomEnd)BoomMax=NewKey;
-           else BoomMin=NewKey;
-           setBoomKeySize();
-       }
-    }
+void Booom::nameSet_1(int in){
+     QByteArray  out;
+             if(in==0)out="玩家_1";
+        else if(in==1)out="幸運電腦_1";
+        else if(in==2)out="普通電腦_1";
+        else if(in==3)out="厄運電腦_1";
+
+    ui->playerName_1->setText( QString::fromLocal8Bit(out));
+    playerType_1=in;
+}
+
+void Booom::nameSet_2(int in){
+     QByteArray  out;
+             if(in==0)out="玩家_2";
+        else if(in==1)out="幸運電腦_2";
+        else if(in==2)out="普通電腦_2";
+        else if(in==3)out="厄運電腦_2";
+
+    ui->playerName_2->setText( QString::fromLocal8Bit(out));
+    playerType_2=in;
+}
+
+void Booom::nameSet_3(int in){
+     QByteArray  out;
+             if(in==0)out="玩家_3";
+        else if(in==1)out="幸運電腦_3";
+        else if(in==2)out="普通電腦_3";
+        else if(in==3)out="厄運電腦_3";
+
+    ui->playerName_3->setText( QString::fromLocal8Bit(out));
+    playerType_3=in;
+}
+
+void Booom::nameSet_4(int in){
+     QByteArray  out;
+             if(in==0)out="玩家_4";
+        else if(in==1)out="幸運電腦_4";
+        else if(in==2)out="普通電腦_4";
+        else if(in==3)out="厄運電腦_4";
+
+    ui->playerName_4->setText( QString::fromLocal8Bit(out));
+    playerType_4=in;
 }
 
 void Booom::on_BoomGOGO_clicked()
@@ -68,7 +140,7 @@ void Booom::on_BoomGOGO_clicked()
            SizeKey= BoomSizeKey.toFloat(),
            SecKey = BoomSecKey.toFloat();
 
-   if(SizeKey>0&&SecKey>0){
+   if(SizeKey>2&&SecKey>0){
        BoomMax = SizeKey;
        BoomSize= SizeKey;
        BoomSec = SecKey;
@@ -80,6 +152,12 @@ void Booom::on_BoomGOGO_clicked()
        if(BoomEnd==0)BoomEnd+=1;
 
        ui->BooomPage->setCurrentIndex(1);
+       ui->BoomKey->setFocus();
+   }
+   else{
+       ui->BoomSizeKey->setText("");
+       ui->BoomSecKey->setText("");
+       ui->BoomSizeKey->setFocus();
    }
 }
 
@@ -91,7 +169,149 @@ void Booom::on_KeySet_clicked()
     int
             NewKey = Key.toFloat();
 
-    KeyIn(NewKey);
+    if(BoomMin<NewKey&&NewKey<BoomMax){
+
+        if(BoomEnd==NewKey)
+            Boooom();
+        else {
+           if(NewKey>BoomEnd)BoomMax=NewKey;
+           else BoomMin=NewKey;
+           setBoomKeySize();
+       }
+    }
+    if(BoomMax-BoomMin<3)
+        WIN();
+    ui->BoomKey->setText("");
 }
 
 
+
+void Booom::on_BoomKey_returnPressed()
+{
+    on_KeySet_clicked();
+}
+
+
+void Booom::on_BoomSizeKey_returnPressed()
+{
+    QString
+               In = ui->BoomSizeKey->text();
+    int
+            NewIn = In.toFloat();
+    if(NewIn>2)
+        ui->BoomSecKey->setFocus();
+    else
+        ui->BoomSizeKey->setText("");
+}
+
+
+void Booom::on_BoomSecKey_returnPressed()
+{
+    QString
+               In = ui->BoomSecKey->text();
+    int
+            NewIn = In.toFloat();
+    if(NewIn<=0)
+        ui->BoomSecKey->setText("");
+}
+
+
+void Booom::on_BoomGOEND_clicked()
+{
+    this->close();
+}
+
+void Booom::on_radioButton_1_clicked()
+{
+    nameSet_1(0);
+}
+
+
+void Booom::on_radioButton_2_clicked()
+{
+    nameSet_1(1);
+}
+
+
+void Booom::on_radioButton_3_clicked()
+{
+    nameSet_1(2);
+}
+
+
+void Booom::on_radioButton_4_clicked()
+{
+    nameSet_1(3);
+}
+
+
+void Booom::on_radioButton_5_clicked()
+{
+    nameSet_2(0);
+}
+
+
+void Booom::on_radioButton_6_clicked()
+{
+    nameSet_2(1);
+}
+
+
+void Booom::on_radioButton_7_clicked()
+{
+    nameSet_2(2);
+}
+
+
+void Booom::on_radioButton_8_clicked()
+{
+    nameSet_2(3);
+}
+
+
+void Booom::on_radioButton_9_clicked()
+{
+    nameSet_3(0);
+}
+
+
+void Booom::on_radioButton_10_clicked()
+{
+    nameSet_3(1);
+}
+
+
+void Booom::on_radioButton_11_clicked()
+{
+    nameSet_3(2);
+}
+
+
+void Booom::on_radioButton_12_clicked()
+{
+    nameSet_3(3);
+}
+
+
+void Booom::on_radioButton_13_clicked()
+{
+    nameSet_4(0);
+}
+
+
+void Booom::on_radioButton_14_clicked()
+{
+    nameSet_4(1);
+}
+
+
+void Booom::on_radioButton_15_clicked()
+{
+    nameSet_4(2);
+}
+
+
+void Booom::on_radioButton_16_clicked()
+{
+    nameSet_4(3);
+}
--- Mini-Games/booom.h
+++ Mini-Games/booom.h
@@ -23,11 +23,56 @@ private slots:
 
     void on_KeySet_clicked();
 
+    void on_BoomKey_returnPressed();
+
+    void on_BoomSizeKey_returnPressed();
+
+    void on_BoomSecKey_returnPressed();
+
+    void on_BoomGOEND_clicked();
+
+
+    void on_radioButton_1_clicked();
+
+    void on_radioButton_2_clicked();
+
+    void on_radioButton_3_clicked();
+
+    void on_radioButton_4_clicked();
+
+    void on_radioButton_5_clicked();
+
+    void on_radioButton_6_clicked();
+
+    void on_radioButton_7_clicked();
+
+    void on_radioButton_8_clicked();
+
+    void on_radioButton_9_clicked();
+
+    void on_radioButton_10_clicked();
+
+    void on_radioButton_11_clicked();
+
+    void on_radioButton_12_clicked();
+
+    void on_radioButton_13_clicked();
+
+    void on_radioButton_14_clicked();
+
+    void on_radioButton_15_clicked();
+
+    void on_radioButton_16_clicked();
+
 private:
     void setBoomKeySize();
 
     void Boooom();
-    void KeyIn(int);
+    void WIN();
+    void nameSet_1(int);
+    void nameSet_2(int);
+    void nameSet_3(int);
+    void nameSet_4(int);
 
     Ui::Booom *ui;
 
@@ -36,6 +81,15 @@ private:
     static int BoomMin;
     static int BoomSec;
     static int BoomEnd;
+    static int playerType_1;
+    static int playerType_2;
+    static int playerType_3;
+    static int playerType_4;
+    static QString play;
+    static QString playName_1;
+    static QString playName_2;
+    static QString playName_3;
+    static QString playName_4;
 };
 
 #endif // BOOOM_H
